ae2a0d188a12f59b9aa74d1418d11d734ce5a7f6
abenikeb <abenikeb79@gmail.com>
2022-09-24 12:08:42 +0300

    modified

--- dist/order.model.js
+++ dist/order.model.js
@@ -1,126 +1,160 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GenerateOrderPrice = exports.OrderNotification = exports.OrderType = exports.OrderReceiverType = exports.OrderPaymentType = exports.OrderStatusState = exports.ORDER_NOTIFICATION_COLLECTION = exports.ORDER_COLLECTION = exports.VAT_PERCENT = void 0;
-const firebase_1 = __importDefault(require("firebase"));
-/** Constants */
-exports.VAT_PERCENT = 0.2;
-/** FIREBASE Collection names */
-exports.ORDER_COLLECTION = "orders";
-exports.ORDER_NOTIFICATION_COLLECTION = "orderNotifications";
-var OrderStatusState;
-(function (OrderStatusState) {
-    OrderStatusState["PENDING"] = "pending";
-    OrderStatusState["ACCEPTED"] = "accepted";
-    OrderStatusState["DELIVERED"] = "delivered";
-    OrderStatusState["CANCLED"] = "cancled";
-})(OrderStatusState = exports.OrderStatusState || (exports.OrderStatusState = {}));
-var OrderPaymentType;
-(function (OrderPaymentType) {
-    OrderPaymentType["STRIPE"] = "stripe";
-})(OrderPaymentType = exports.OrderPaymentType || (exports.OrderPaymentType = {}));
-var OrderReceiverType;
-(function (OrderReceiverType) {
-    OrderReceiverType["BUYER"] = "buyer";
-    OrderReceiverType["SELLER"] = "seller";
-})(OrderReceiverType = exports.OrderReceiverType || (exports.OrderReceiverType = {}));
-class OrderType {
-    constructor(order) {
-        var _a;
-        this.orderDate = firebase_1.default.firestore.Timestamp.now();
-        this.id = order.id;
-        this.orderNumber = order.orderNumber;
-        this.buyer = order.buyer;
-        this.seller = order.seller;
-        // this.deliveryDate = order.deliveryDate
-        this.product = order.product;
-        // this.location = order.location
-        this.paymentType = order.paymentType;
-        this.sellerId = (_a = order.seller) === null || _a === void 0 ? void 0 : _a.id;
-        this.orderDate = order.orderDate;
-        this.status = order.status || OrderStatusState.PENDING;
-        this.status = order.status || OrderStatusState.PENDING;
-        this.actualCarbonReductionPrice =
-            order.product.product.carbonReduction * order.product.quantity;
-        /** Calculate the default price from the total price */
-        this.price = (0, exports.GenerateOrderPrice)(
-        // Calculate Net Price
-        order.product.product.price * order.product.quantity, this.calculateCarbonReduction(order.product.product.carbonReduction * order.product.quantity));
-        this.buyerId = order.buyerId;
-    }
-    getOrderDate() {
-        if (!this.orderDate)
-            throw new Error("Order doesn't have order date.");
-        return this.orderDate.toDate();
-    }
-    getFormattedDate() {
-        var _a;
-        if (!this.orderDate)
-            throw new Error("Order doesn't have order date.");
-        return (_a = this.getOrderDate()) === null || _a === void 0 ? void 0 : _a.toLocaleString();
-    }
-    getActualCarbonReuctionPrice() {
-        return this.actualCarbonReductionPrice;
-    }
-    calculateCarbonReduction(price) {
-        return price - price * 0.01;
-    }
-    toJSON(withId = false) {
-        var _a;
-        const orderPrice = this.price;
-        if (typeof orderPrice.grossPrice === "undefined")
-            throw new Error("Order gross price shouldn't be undefined");
-        const output = {
-            status: (_a = this.status) === null || _a === void 0 ? void 0 : _a.toString(),
-            orderDate: typeof this.orderDate === "undefined"
-                ? new Date(Date.now())
-                : this.orderDate,
-            orderNumber: this.orderNumber,
-            sellerId: this.sellerId,
-            buyerId: this.buyerId,
-            product: Object.assign({}, this.product),
-            paymentType: this.paymentType,
-            price: {
-                carbonReduction: +orderPrice.carbonReduction,
-                netPrice: +orderPrice.netPrice,
-                addedTax: +orderPrice.addedTax,
-                grossPrice: +orderPrice.grossPrice,
-            },
-            buyer: Object.assign({}, this.buyer),
-            seller: Object.assign({}, this.seller),
-        };
-        return withId ? Object.assign({ id: this.id }, output) : output;
-    }
-}
-exports.OrderType = OrderType;
-class OrderNotification {
-    constructor(orderNotification) {
-        this.type = exports.ORDER_NOTIFICATION_COLLECTION;
-        this.orderId = orderNotification.orderId;
-        this.userId = orderNotification.userId;
-        this.message = orderNotification.message;
-        this.receiverId = orderNotification.userId;
-        this.status = 1;
-        this.content = orderNotification.message;
-        this.receiverType = orderNotification.receiverType;
-        this.dateTime = firebase_1.default.firestore.Timestamp.now();
-        this.isRead = orderNotification.isRead || false;
-    }
-    getType() {
-        return this.type;
-    }
-}
-exports.OrderNotification = OrderNotification;
-// Helpers
-const GenerateOrderPrice = (netPrice, carbonReduction) => {
-    const orderPrice = {
-        netPrice,
-        addedTax: exports.VAT_PERCENT * netPrice,
-        carbonReduction,
-    };
-    orderPrice.grossPrice = orderPrice.addedTax + orderPrice.netPrice;
-    return orderPrice;
-};
-exports.GenerateOrderPrice = GenerateOrderPrice;
+// // import firebase from "firebase";
+// import { ProductType } from "./products.model";
+// /** Constants */
+// export const VAT_PERCENT = 0.2;
+// /** FIREBASE Collection names */
+// export const ORDER_COLLECTION = "orders";
+// export const ORDER_NOTIFICATION_COLLECTION = "orderNotifications";
+// export interface UserType {
+//   id: string;
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+// }
+// export enum OrderStatusState {
+//   PENDING = "pending",
+//   ACCEPTED = "accepted",
+//   DELIVERED = "delivered",
+//   CANCLED = "cancled",
+// }
+// export enum OrderPaymentType {
+//   STRIPE = "stripe",
+// }
+// export enum OrderReceiverType {
+//   BUYER = "buyer",
+//   SELLER = "seller",
+// }
+// export interface OrderProduct {
+//   product: ProductType;
+//   quantity: number;
+// }
+// export interface OrderPrice {
+//   carbonReduction: number;
+//   netPrice: number;
+//   addedTax: number;
+//   grossPrice?: number;
+// }
+// export class OrderType {
+//   id?: string;
+//   orderNumber: number;
+//   status?: OrderStatusState;
+//   orderDate?: firebase.firestore.Timestamp = firebase.firestore.Timestamp.now();
+//   sellerId?: string;
+//   buyerId?: string;
+//   product: OrderProduct;
+//   paymentType: OrderPaymentType;
+//   price?: OrderPrice;
+//   buyer?: UserType;
+//   seller?: UserType;
+//   private actualCarbonReductionPrice: number;
+//   constructor(order: OrderType) {
+//     this.id = order.id;
+//     this.orderNumber = order.orderNumber;
+//     this.buyer = order.buyer;
+//     this.seller = order.seller;
+//     // this.deliveryDate = order.deliveryDate
+//     this.product = order.product;
+//     // this.location = order.location
+//     this.paymentType = order.paymentType;
+//     this.sellerId = order.seller?.id;
+//     this.orderDate = order.orderDate;
+//     this.status = order.status || OrderStatusState.PENDING;
+//     this.status = order.status || OrderStatusState.PENDING;
+//     this.actualCarbonReductionPrice =
+//       order.product.product.carbonReduction * order.product.quantity;
+//     /** Calculate the default price from the total price */
+//     this.price = GenerateOrderPrice(
+//       // Calculate Net Price
+//       order.product.product.price * order.product.quantity,
+//       this.calculateCarbonReduction(
+//         order.product.product.carbonReduction * order.product.quantity
+//       )
+//     );
+//     this.buyerId = order.buyerId;
+//   }
+//   getOrderDate(): Date | undefined {
+//     if (!this.orderDate) throw new Error("Order doesn't have order date.");
+//     return this.orderDate.toDate();
+//   }
+//   getFormattedDate(): string | undefined {
+//     if (!this.orderDate) throw new Error("Order doesn't have order date.");
+//     return this.getOrderDate()?.toLocaleString();
+//   }
+//   getActualCarbonReuctionPrice() {
+//     return this.actualCarbonReductionPrice;
+//   }
+//   private calculateCarbonReduction(price: number) {
+//     return price - price * 0.01;
+//   }
+//   toJSON(withId: boolean = false): object {
+//     const orderPrice = this.price as OrderPrice;
+//     if (typeof orderPrice.grossPrice === "undefined")
+//       throw new Error("Order gross price shouldn't be undefined");
+//     const output = {
+//       status: this.status?.toString(),
+//       orderDate:
+//         typeof this.orderDate === "undefined"
+//           ? new Date(Date.now())
+//           : this.orderDate,
+//       orderNumber: this.orderNumber,
+//       sellerId: this.sellerId,
+//       buyerId: this.buyerId,
+//       product: { ...this.product },
+//       paymentType: this.paymentType,
+//       price: {
+//         carbonReduction: +orderPrice.carbonReduction,
+//         netPrice: +orderPrice.netPrice,
+//         addedTax: +orderPrice.addedTax,
+//         grossPrice: +orderPrice.grossPrice,
+//       },
+//       buyer: { ...this.buyer },
+//       seller: { ...this.seller },
+//     };
+//     return withId ? { id: this.id, ...output } : output;
+//   }
+// }
+// export interface OrderState {
+//   orderNotifications?: Array<any>;
+//   orders?: Array<OrderType>;
+// }
+// export class OrderNotification {
+//   id?: string;
+//   orderId: string;
+//   userId: string;
+//   message: string;
+//   isRead?: boolean;
+//   type?: Readonly<string> = ORDER_NOTIFICATION_COLLECTION;
+//   receiverId?: string;
+//   status?: number;
+//   content?: string;
+//   receiverType: OrderReceiverType;
+//   dateTime?: firebase.firestore.Timestamp;
+//   constructor(orderNotification: OrderNotification) {
+//     this.orderId = orderNotification.orderId;
+//     this.userId = orderNotification.userId;
+//     this.message = orderNotification.message;
+//     this.receiverId = orderNotification.userId;
+//     this.status = 1;
+//     this.content = orderNotification.message;
+//     this.receiverType = orderNotification.receiverType;
+//     this.dateTime = firebase.firestore.Timestamp.now();
+//     this.isRead = orderNotification.isRead || false;
+//   }
+//   getType?() {
+//     return this.type;
+//   }
+// }
+// // Helpers
+// export const GenerateOrderPrice = (
+//   netPrice: number,
+//   carbonReduction: number
+// ): OrderPrice => {
+//   const orderPrice: OrderPrice = {
+//     netPrice,
+//     addedTax: VAT_PERCENT * netPrice,
+//     carbonReduction,
+//   };
+//   orderPrice.grossPrice = orderPrice.addedTax + orderPrice.netPrice;
+//   return orderPrice;
+// };
--- order.model.ts
+++ order.model.ts
@@ -1,189 +1,189 @@
-import firebase from "firebase";
-import { ProductType } from "./products.model";
+// // import firebase from "firebase";
+// import { ProductType } from "./products.model";
 
-/** Constants */
-export const VAT_PERCENT = 0.2;
+// /** Constants */
+// export const VAT_PERCENT = 0.2;
 
-/** FIREBASE Collection names */
-export const ORDER_COLLECTION = "orders";
-export const ORDER_NOTIFICATION_COLLECTION = "orderNotifications";
+// /** FIREBASE Collection names */
+// export const ORDER_COLLECTION = "orders";
+// export const ORDER_NOTIFICATION_COLLECTION = "orderNotifications";
 
-export interface UserType {
-  id: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-}
+// export interface UserType {
+//   id: string;
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+// }
 
-export enum OrderStatusState {
-  PENDING = "pending",
-  ACCEPTED = "accepted",
-  DELIVERED = "delivered",
-  CANCLED = "cancled",
-}
+// export enum OrderStatusState {
+//   PENDING = "pending",
+//   ACCEPTED = "accepted",
+//   DELIVERED = "delivered",
+//   CANCLED = "cancled",
+// }
 
-export enum OrderPaymentType {
-  STRIPE = "stripe",
-}
+// export enum OrderPaymentType {
+//   STRIPE = "stripe",
+// }
 
-export enum OrderReceiverType {
-  BUYER = "buyer",
-  SELLER = "seller",
-}
+// export enum OrderReceiverType {
+//   BUYER = "buyer",
+//   SELLER = "seller",
+// }
 
-export interface OrderProduct {
-  product: ProductType;
-  quantity: number;
-}
+// export interface OrderProduct {
+//   product: ProductType;
+//   quantity: number;
+// }
 
-export interface OrderPrice {
-  carbonReduction: number;
-  netPrice: number;
-  addedTax: number;
-  grossPrice?: number;
-}
+// export interface OrderPrice {
+//   carbonReduction: number;
+//   netPrice: number;
+//   addedTax: number;
+//   grossPrice?: number;
+// }
 
-export class OrderType {
-  id?: string;
-  orderNumber: number;
-  status?: OrderStatusState;
-  orderDate?: firebase.firestore.Timestamp = firebase.firestore.Timestamp.now();
-  sellerId?: string;
-  buyerId?: string;
-  product: OrderProduct;
-  paymentType: OrderPaymentType;
-  price?: OrderPrice;
-  buyer?: UserType;
-  seller?: UserType;
+// export class OrderType {
+//   id?: string;
+//   orderNumber: number;
+//   status?: OrderStatusState;
+//   orderDate?: firebase.firestore.Timestamp = firebase.firestore.Timestamp.now();
+//   sellerId?: string;
+//   buyerId?: string;
+//   product: OrderProduct;
+//   paymentType: OrderPaymentType;
+//   price?: OrderPrice;
+//   buyer?: UserType;
+//   seller?: UserType;
 
-  private actualCarbonReductionPrice: number;
+//   private actualCarbonReductionPrice: number;
 
-  constructor(order: OrderType) {
-    this.id = order.id;
-    this.orderNumber = order.orderNumber;
-    this.buyer = order.buyer;
-    this.seller = order.seller;
-    // this.deliveryDate = order.deliveryDate
-    this.product = order.product;
-    // this.location = order.location
-    this.paymentType = order.paymentType;
-    this.sellerId = order.seller?.id;
-    this.orderDate = order.orderDate;
+//   constructor(order: OrderType) {
+//     this.id = order.id;
+//     this.orderNumber = order.orderNumber;
+//     this.buyer = order.buyer;
+//     this.seller = order.seller;
+//     // this.deliveryDate = order.deliveryDate
+//     this.product = order.product;
+//     // this.location = order.location
+//     this.paymentType = order.paymentType;
+//     this.sellerId = order.seller?.id;
+//     this.orderDate = order.orderDate;
 
-    this.status = order.status || OrderStatusState.PENDING;
+//     this.status = order.status || OrderStatusState.PENDING;
 
-    this.status = order.status || OrderStatusState.PENDING;
+//     this.status = order.status || OrderStatusState.PENDING;
 
-    this.actualCarbonReductionPrice =
-      order.product.product.carbonReduction * order.product.quantity;
+//     this.actualCarbonReductionPrice =
+//       order.product.product.carbonReduction * order.product.quantity;
 
-    /** Calculate the default price from the total price */
-    this.price = GenerateOrderPrice(
-      // Calculate Net Price
-      order.product.product.price * order.product.quantity,
-      this.calculateCarbonReduction(
-        order.product.product.carbonReduction * order.product.quantity
-      )
-    );
-    this.buyerId = order.buyerId;
-  }
+//     /** Calculate the default price from the total price */
+//     this.price = GenerateOrderPrice(
+//       // Calculate Net Price
+//       order.product.product.price * order.product.quantity,
+//       this.calculateCarbonReduction(
+//         order.product.product.carbonReduction * order.product.quantity
+//       )
+//     );
+//     this.buyerId = order.buyerId;
+//   }
 
-  getOrderDate(): Date | undefined {
-    if (!this.orderDate) throw new Error("Order doesn't have order date.");
-    return this.orderDate.toDate();
-  }
+//   getOrderDate(): Date | undefined {
+//     if (!this.orderDate) throw new Error("Order doesn't have order date.");
+//     return this.orderDate.toDate();
+//   }
 
-  getFormattedDate(): string | undefined {
-    if (!this.orderDate) throw new Error("Order doesn't have order date.");
-    return this.getOrderDate()?.toLocaleString();
-  }
+//   getFormattedDate(): string | undefined {
+//     if (!this.orderDate) throw new Error("Order doesn't have order date.");
+//     return this.getOrderDate()?.toLocaleString();
+//   }
 
-  getActualCarbonReuctionPrice() {
-    return this.actualCarbonReductionPrice;
-  }
+//   getActualCarbonReuctionPrice() {
+//     return this.actualCarbonReductionPrice;
+//   }
 
-  private calculateCarbonReduction(price: number) {
-    return price - price * 0.01;
-  }
+//   private calculateCarbonReduction(price: number) {
+//     return price - price * 0.01;
+//   }
 
-  toJSON(withId: boolean = false): object {
-    const orderPrice = this.price as OrderPrice;
+//   toJSON(withId: boolean = false): object {
+//     const orderPrice = this.price as OrderPrice;
 
-    if (typeof orderPrice.grossPrice === "undefined")
-      throw new Error("Order gross price shouldn't be undefined");
+//     if (typeof orderPrice.grossPrice === "undefined")
+//       throw new Error("Order gross price shouldn't be undefined");
 
-    const output = {
-      status: this.status?.toString(),
-      orderDate:
-        typeof this.orderDate === "undefined"
-          ? new Date(Date.now())
-          : this.orderDate,
-      orderNumber: this.orderNumber,
-      sellerId: this.sellerId,
-      buyerId: this.buyerId,
-      product: { ...this.product },
-      paymentType: this.paymentType,
-      price: {
-        carbonReduction: +orderPrice.carbonReduction,
-        netPrice: +orderPrice.netPrice,
-        addedTax: +orderPrice.addedTax,
-        grossPrice: +orderPrice.grossPrice,
-      },
-      buyer: { ...this.buyer },
-      seller: { ...this.seller },
-    };
+//     const output = {
+//       status: this.status?.toString(),
+//       orderDate:
+//         typeof this.orderDate === "undefined"
+//           ? new Date(Date.now())
+//           : this.orderDate,
+//       orderNumber: this.orderNumber,
+//       sellerId: this.sellerId,
+//       buyerId: this.buyerId,
+//       product: { ...this.product },
+//       paymentType: this.paymentType,
+//       price: {
+//         carbonReduction: +orderPrice.carbonReduction,
+//         netPrice: +orderPrice.netPrice,
+//         addedTax: +orderPrice.addedTax,
+//         grossPrice: +orderPrice.grossPrice,
+//       },
+//       buyer: { ...this.buyer },
+//       seller: { ...this.seller },
+//     };
 
-    return withId ? { id: this.id, ...output } : output;
-  }
-}
-export interface OrderState {
-  orderNotifications?: Array<any>;
-  orders?: Array<OrderType>;
-}
+//     return withId ? { id: this.id, ...output } : output;
+//   }
+// }
+// export interface OrderState {
+//   orderNotifications?: Array<any>;
+//   orders?: Array<OrderType>;
+// }
 
-export class OrderNotification {
-  id?: string;
-  orderId: string;
-  userId: string;
-  message: string;
-  isRead?: boolean;
-  type?: Readonly<string> = ORDER_NOTIFICATION_COLLECTION;
-  receiverId?: string;
-  status?: number;
-  content?: string;
-  receiverType: OrderReceiverType;
-  dateTime?: firebase.firestore.Timestamp;
+// export class OrderNotification {
+//   id?: string;
+//   orderId: string;
+//   userId: string;
+//   message: string;
+//   isRead?: boolean;
+//   type?: Readonly<string> = ORDER_NOTIFICATION_COLLECTION;
+//   receiverId?: string;
+//   status?: number;
+//   content?: string;
+//   receiverType: OrderReceiverType;
+//   dateTime?: firebase.firestore.Timestamp;
 
-  constructor(orderNotification: OrderNotification) {
-    this.orderId = orderNotification.orderId;
-    this.userId = orderNotification.userId;
-    this.message = orderNotification.message;
-    this.receiverId = orderNotification.userId;
-    this.status = 1;
-    this.content = orderNotification.message;
-    this.receiverType = orderNotification.receiverType;
-    this.dateTime = firebase.firestore.Timestamp.now();
+//   constructor(orderNotification: OrderNotification) {
+//     this.orderId = orderNotification.orderId;
+//     this.userId = orderNotification.userId;
+//     this.message = orderNotification.message;
+//     this.receiverId = orderNotification.userId;
+//     this.status = 1;
+//     this.content = orderNotification.message;
+//     this.receiverType = orderNotification.receiverType;
+//     this.dateTime = firebase.firestore.Timestamp.now();
 
-    this.isRead = orderNotification.isRead || false;
-  }
+//     this.isRead = orderNotification.isRead || false;
+//   }
 
-  getType?() {
-    return this.type;
-  }
-}
+//   getType?() {
+//     return this.type;
+//   }
+// }
 
-// Helpers
-export const GenerateOrderPrice = (
-  netPrice: number,
-  carbonReduction: number
-): OrderPrice => {
-  const orderPrice: OrderPrice = {
-    netPrice,
-    addedTax: VAT_PERCENT * netPrice,
-    carbonReduction,
-  };
+// // Helpers
+// export const GenerateOrderPrice = (
+//   netPrice: number,
+//   carbonReduction: number
+// ): OrderPrice => {
+//   const orderPrice: OrderPrice = {
+//     netPrice,
+//     addedTax: VAT_PERCENT * netPrice,
+//     carbonReduction,
+//   };
 
-  orderPrice.grossPrice = orderPrice.addedTax + orderPrice.netPrice;
+//   orderPrice.grossPrice = orderPrice.addedTax + orderPrice.netPrice;
 
-  return orderPrice;
-};
+//   return orderPrice;
+// };
