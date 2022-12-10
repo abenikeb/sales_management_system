4517819946b6eb45e5ae8655f97c392107670d88
abenikeb <abenikeb79@gmail.com>
2022-12-10 16:11:37 +0300

    modified:   service/DataBase.ts

--- service/DataBase.ts
+++ service/DataBase.ts
@@ -3,32 +3,32 @@ require("dotenv").config();
 
 // local DB Connect
 
-const pool = new Pool({
-  user: process.env.DB_USER as string,
-  password: process.env.DB_PASSWORD as string,
-  host: process.env.DB_HOST as string,
-  port: process.env.DB_PORT as any,
-  database: process.env.DB_NAME as string,
-});
+// const pool = new Pool({
+//   user: process.env.DB_USER as string,
+//   password: process.env.DB_PASSWORD as string,
+//   host: process.env.DB_HOST as string,
+//   port: process.env.DB_PORT as any,
+//   database: process.env.DB_NAME as string,
+// });
 
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error("Error acquiring client", err.stack);
-  }
-});
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error("Error acquiring client", err.stack);
+//   }
+// });
 
 // HEROKU DB Connect
 
-// const { Client } = require("pg");
+const { Client } = require("pg");
 
-// const pool = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const pool = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-// pool.connect();
+pool.connect();
 
 // pool.query(
 //   "SELECT table_schema,table_name FROM information_schema.tables;",
