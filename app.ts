1d4339fadbd8a4448155bef60a9e13a8cd5ba235
abenikeb <abenikeb79@gmail.com>
2022-12-11 00:04:44 +0300

    adding

--- app.ts
+++ app.ts
@@ -1,15 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const App = require("./service/ExpressApp.js");
-// import DB from "./service/DataBase";
+const App = require("./service/ExpressApp");
 
 const app = express();
 
 async function startServer() {
   await App(app);
-  // const port = process.env.PORT || 5000;
   const port = 5000;
-  // const port = 5000;
+
   app.listen(port, () => {
     console.log(`Server is listing on Port ${port}`);
   });
--- dist/app.js
+++ dist/app.js
@@ -8,20 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
-const express_1 = __importDefault(require("express"));
-const ExpressApp_1 = __importDefault(require("./service/ExpressApp"));
-const app = (0, express_1.default)();
+const express = require("express");
+const App = require("./service/ExpressApp");
+const app = express();
 function startServer() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield (0, ExpressApp_1.default)(app);
-        // const port = process.env.PORT || 5000;
+        yield App(app);
         const port = 5000;
-        // const port = 5000;
         app.listen(port, () => {
             console.log(`Server is listing on Port ${port}`);
         });
